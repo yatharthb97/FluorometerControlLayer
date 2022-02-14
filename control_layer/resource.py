5f97c61b48dbea42b7294022a5cf189ce3ea6dfa
Yatharth <yatharth1997@gmail.com>
2022-02-14 17:46:59 +0530

    Added extract concentration function

--- control_layer/resource.py
+++ control_layer/resource.py
@@ -22,3 +22,15 @@ def is_number(string):
         return True
     except ValueError:
         return False
+
+def extract_conc(string, no_samples):
+
+	string = string.replace(" ", "") #Clean Spaces
+	string = string.replace('\n', "") #Clean any endline
+	splitted = string.split(',')
+	length = len(splitted)
+	if(length < no_samples):
+		splitted.extend(["#"]*(no_samples-length))
+	return splitted[:no_samples]
+			
+
